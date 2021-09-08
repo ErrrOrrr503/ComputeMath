07bfaa042ee47b392d6cbdd0462e0ed97c98224f
ErrrOrrr503 <egortit-33@yandex.ru>
2021-09-09 01:07:08 +0300

    added d2F and mistakes

--- dF Graph/main.py
+++ dF Graph/main.py
@@ -1,23 +1,28 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python3
 import matplotlib.pyplot as plt
 import numpy as np
 
 #<functions>
 def F (x_data):
     assert (isinstance (x_data, np.ndarray))
-    return np.exp (x_data)
+    return np.sin (x_data)
 
 def dF (x_data):
     assert (isinstance (x_data, np.ndarray))
-    return np.exp (x_data)
+    return np.cos (x_data)
+
+def d2F (x_data):
+    assert (isinstance (x_data, np.ndarray))
+    return -np.sin (x_data)
 #</functions>
 
 #<dimensions>
 x_low = -5
 x_high = 5
-y_low = -0.5
-y_high = 100
+y_low = -1.5
+y_high = 1.5
 x_num = 20
+x_num_max = 100
 x_data = np.linspace (x_low, x_high, x_num)
 x_delta = (x_high - x_low) / (x_num - 1)
 #</dimensions>
@@ -26,18 +31,20 @@ x_delta = (x_high - x_low) / (x_num - 1)
 graph_title = "Graph"
 x_label = ""
 y_label = ""
-plt.axis ([x_low, x_high, y_low, y_high])
 plt.title (graph_title)
 plt.xlabel (x_label)
 plt.ylabel (y_label)
-plt.grid (True)
 #</style>
 
-#<presize d(sin(x))/dx>
-plt.plot (x_data, dF (x_data), 'b-')
-#</presize d(sin(x))/dx>
+#<dF/dx>
+plt.subplot (221) # 2 line 2 graphs in line 
+plt.grid (True)
 
-#<O(n) right (Fi+1 - Fi) / h>
+#<presize dF/dx>
+plt.plot (x_data, dF (x_data), 'b-')
+#</presize dF/dx>
+
+#<O(h) right (Fi+1 - Fi) / h>
 def Oright_dy (x_data, y_data): # differentiating net-function (сеточная ф-ция)
     assert (isinstance (x_data, np.ndarray))
     assert (isinstance (y_data, np.ndarray))
@@ -50,9 +57,9 @@ def Oright_dy (x_data, y_data): # differentiating net-function (сеточная
     return dy
 
 plt.plot (x_data, Oright_dy (x_data, F (x_data)), 'g-')
-#</O(n) right (Fi+1 - Fi) / h>
+#</O(h) right (Fi+1 - Fi) / h>
 
-#<O(n) left (Fi - Fi-1) / h>
+#<O(h) left (Fi - Fi-1) / h>
 def Oleft_dy (x_data, y_data): # differentiating net-function (сеточная ф-ция)
     assert (isinstance (x_data, np.ndarray))
     assert (isinstance (y_data, np.ndarray))
@@ -65,10 +72,10 @@ def Oleft_dy (x_data, y_data): # differentiating net-function (сеточная 
     return dy
 
 plt.plot (x_data, Oleft_dy (x_data, F (x_data)), 'r-')
-#</O(n) left (Fi - Fi-1) / h>
+#</O(h) left (Fi - Fi-1) / h>
 
-#<O(n2) (Fi+1 - Fi-1) / 2h; F'0 = (-3Fi + 4Fi+1 - Fi+2) / 2h; F'n = -(-3Fi + 4Fi-1 - Fi-2) / 2h>
-def Oleft_dy (x_data, y_data): # differentiating net-function (сеточная ф-ция)
+#<O(h2) (Fi+1 - Fi-1) / 2h; F'0 = (-3Fi + 4Fi+1 - Fi+2) / 2h; F'n = -(-3Fi + 4Fi-1 - Fi-2) / 2h>
+def O2_dy (x_data, y_data): # differentiating net-function (сеточная ф-ция)
     assert (isinstance (x_data, np.ndarray))
     assert (isinstance (y_data, np.ndarray))
     assert (x_data.size == y_data.size)
@@ -80,7 +87,84 @@ def Oleft_dy (x_data, y_data): # differentiating net-function (сеточная 
     dy[len-1] = -(-3 * y_data[len - 1] + 4 * y_data[len - 2] - y_data[len - 3]) / (x_data[len - 1] - x_data[len - 3])
     return dy
 
-plt.plot (x_data, Oleft_dy (x_data, F (x_data)), 'm-')
-#</O(n2) (Fi+1 - Fi-1) / 2h; F'0 = (-3Fi + 4Fi+1 - Fi+2) / 2h; F'n = -(-3Fi + 4Fi-1 - Fi-2) / 2h>
+plt.plot (x_data, O2_dy (x_data, F (x_data)), 'm-')
+#</O(h2) (Fi+1 - Fi-1) / 2h; F'0 = (-3Fi + 4Fi+1 - Fi+2) / 2h; F'n = -(-3Fi + 4Fi-1 - Fi-2) / 2h>
+#</dF/dx>
+
+#<d2F/dx>
+#<presize d2F/dx>
+plt.subplot (222)
+plt.grid (True)
+plt.plot (x_data, d2F (x_data), 'b-')
+#</presize d2F/dx>
+
+#<O2(h) (dFi+1 - 2dFi + 2dFi-1) / h^2>
+def O2_d2y_fixed_h (x_data, y_data):
+    assert (isinstance (x_data, np.ndarray))
+    assert (isinstance (y_data, np.ndarray))
+    assert (x_data.size == y_data.size)
+    len = x_data.size
+    h2 = (x_data[1] - x_data[0]) * (x_data[1] - x_data[0])
+    d2y = np.empty (len)
+    for i in range (1, len - 1):
+        d2y[i] = (y_data[i + 1] + y_data[i - 1] - 2 * y_data[i]) / h2
+    d2y[len - 1] = (2 * y_data[len - 1] - 5 * y_data[len - 2] + 4 * y_data[len - 3] - y_data[len - 4]) / h2
+    d2y[0] = (2 * y_data[0] - 5 * y_data[1] + 4 * y_data[2] - y_data[3]) / h2
+    return d2y
+
+plt.subplot (222)
+plt.grid (True)
+plt.plot (x_data, O2_d2y_fixed_h (x_data, F (x_data)), 'm-')
+#</O2(h) (dFi+1 - 2dFi + 2dFi-1) / h^2>
+#</d2F/dx>
+
+#<F mistake D = max |F(i) - Fcalculated(i)|>
+def D_y (x_data, y_data, y0_data):
+    assert (isinstance (x_data, np.ndarray))
+    assert (isinstance (y0_data, np.ndarray))
+    assert (isinstance (y_data, np.ndarray))
+    assert (x_data.size == y0_data.size == y_data.size)
+    len = x_data.size
+    D_y = -1
+    for i in range (0, len):
+        if D_y < abs (y_data[i] - y0_data[i]):
+            D_y = abs (y_data[i] - y0_data[i])
+    return D_y
+#</F mistake D = max |F(i) - Fcalculated(i)|>
+
+#<mistake graphs>
+n_data = np.arange (4, x_num_max) #4 is minimal due to o2
+dy_data = np.empty (x_num_max - 4)
+h_data = (x_high - x_low) / n_data
+
+#<dF mistake graph>
+plt.subplot (223)
+plt.grid (True)
+plt.xscale ('log')
+for i in range (4, x_num_max):
+    temp_x_data = np.linspace (x_low, x_high, i)
+    dy_data[i - 4] = D_y (temp_x_data, Oleft_dy (temp_x_data, F (temp_x_data)), dF (temp_x_data))
+plt.plot (n_data, dy_data, 'ro')
+for i in range (4, x_num_max):
+    temp_x_data = np.linspace (x_low, x_high, i)
+    dy_data[i - 4] = D_y (temp_x_data, Oright_dy (temp_x_data, F (temp_x_data)), dF (temp_x_data))
+plt.plot (n_data, dy_data, 'g.')
+for i in range (4, x_num_max):
+    temp_x_data = np.linspace (x_low, x_high, i)
+    dy_data[i - 4] = D_y (temp_x_data, O2_dy (temp_x_data, F (temp_x_data)), dF (temp_x_data))
+plt.plot (n_data, dy_data, 'm-')
+plt.plot (n_data, h_data, 'c-')
+#</dF mistake graph>
+
+#<d2F mistake graph>
+plt.subplot (224)
+plt.grid (True)
+plt.xscale ('log')
+for i in range (4, x_num_max):
+    temp_x_data = np.linspace (x_low, x_high, i)
+    dy_data[i - 4] = D_y (temp_x_data, O2_d2y_fixed_h (temp_x_data, F (temp_x_data)), d2F (temp_x_data))
+plt.plot (n_data, dy_data, 'm-')
+plt.plot (n_data, h_data, 'c-')
+#</d2F mistake graph>
 
 plt.show ()
