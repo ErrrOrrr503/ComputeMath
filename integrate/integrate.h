f3f0284c101024d8b6d58f843ab763ab231141e2
ErrrOrrr503 <egortit-33@yandex.ru>
2021-12-14 00:35:53 +0300

    integration lab 5 added

---
+++ integrate/integrate.h
@@ -0,0 +1,289 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+
+enum left_right {
+    improper_right,
+    improper_left
+};
+
+template <typename num_t>
+class netfunc {
+public:
+    num_t (*ref_func) (num_t);
+    num_t (*eps_func) (num_t);
+    size_t netsize;
+    std::vector<num_t> x_data;
+    std::vector<num_t> f_data;
+    num_t h;
+    num_t x_max;
+    num_t x_min;
+    bool has_reference;
+    left_right end;
+    bool is_improper;
+
+    netfunc () = delete;
+    netfunc (num_t (*reference_func) (num_t), num_t min, num_t max, size_t netsz) : 
+        ref_func (reference_func), 
+        eps_func (NULL),
+        x_max (max), x_min (min), 
+        has_reference (1),
+        is_improper (0)
+    {
+        build_net (netsz);
+    }
+    netfunc (num_t (*reference_func) (num_t), num_t (*epsilon_func) (num_t), num_t min, num_t max, left_right feature_end) : 
+        ref_func (reference_func), 
+        eps_func (epsilon_func), 
+        x_max (max), x_min (min), 
+        has_reference (1), 
+        end (feature_end),
+        is_improper (1)
+    {
+        if ((isnanl (x_max) && end != improper_right) || (isnanl (x_min) && end != improper_left))
+            throw std::runtime_error ("proper end must correspond to nan");
+    }
+    void build_net (size_t netsz)
+    {
+        if (netsz < 2)
+            throw std::runtime_error ("number of points in netsize must be greater than 1");
+        netsize = netsz;
+        h = (x_max - x_min) / (netsz - 1);
+        for (size_t i = 0; i < netsz; i++) {
+            x_data.push_back (x_min + (num_t) (i) / (num_t) (netsz - 1) * (x_max - x_min));
+            f_data.push_back (ref_func (x_data[i]));
+        }
+    }
+    ~netfunc ()
+    {
+    }
+};
+
+template <typename num_t>
+class integrator {
+private:
+    netfunc<num_t> func;
+    bool use_runge = 0;
+    size_t max_iterations = 20;
+    num_t precision = 0;
+    std::string errstr;
+
+public:
+    num_t ans = 0;
+    num_t runge = 0;
+    size_t iterations = 0;
+    num_t improper_edge = 0;
+
+    integrator () = delete;
+    integrator (netfunc<num_t> f) : func (f)
+    {
+    }
+    ~integrator ()
+    {
+    }
+
+    void assume_pure_net ()
+    {
+        func.has_reference = 0;
+        use_runge = 0;
+    }
+
+    const std::string & get_err ()
+    {
+        return errstr;
+    }
+
+    int enable_runge (num_t precision)
+    {
+        if (func.has_reference) {
+            use_runge = 1;
+            this->precision = precision;
+            return 0;
+        }
+        else {
+            errstr = "func is pure net, runge needs more data";
+            return 1;
+        }
+    }
+
+    int integrate_rectangles ()
+    {
+        if (func.is_improper) {
+            errstr = "improper must use improper methods";
+            return 1;
+        }
+        num_t I = 0;
+        if (func.has_reference) {
+            for (size_t i = 0; i < func.netsize - 1; i++)
+                I += func.ref_func ((func.x_data[i] + func.x_data[i + 1]) / 2);
+            I *= func.h;
+            if (use_runge) {
+                num_t I_2h = I;
+                I = 0;
+                size_t old_netsize = func.netsize;
+                func.build_net (func.netsize + func.netsize - 1); // x2 number of h-s
+                for (size_t i = 0; i < func.netsize - 1; i++)
+                    I += func.ref_func ((func.x_data[i] + func.x_data[i + 1]) / 2);
+                I *= func.h;
+                for (size_t i = 0; i < max_iterations && precision < fabsl ((I - I_2h)); i++) {
+                    I_2h = I;
+                    I = 0;
+                    func.build_net (func.netsize + func.netsize - 1);
+                    for (size_t i = 0; i < func.netsize - 1; i++)
+                        I += func.ref_func ((func.x_data[i] + func.x_data[i + 1]) / 2);
+                    I *= func.h;
+                    iterations = i + 2;
+                }
+                func.build_net (old_netsize);
+                runge = (I - I_2h) / 3;
+                I += runge;
+            }
+        }
+        else {
+            for (size_t i = 0; i < func.netsize - 1; i += 2)
+                I += (func.f_data[i + 1]);
+            I *= 2 * func.h;
+        }
+        ans = I;
+        return 0;
+    }
+
+    int integrate_trapezoids ()
+    {
+        if (func.is_improper) {
+            errstr = "improper must use improper methods";
+            return 1;
+        }
+        num_t I = 0;
+        for (size_t i = 0; i < func.netsize - 1; i++)
+            I += (func.f_data[i] + func.f_data[i + 1]);
+        I *= func.h / 2;
+        if (func.has_reference && use_runge) {
+            num_t I_2h = I;
+            I = 0;
+            size_t old_netsize = func.netsize;
+            func.build_net (func.netsize + func.netsize - 1); // x2 number of h-s
+            for (size_t i = 0; i < func.netsize - 1; i++)
+                I += (func.f_data[i] + func.f_data[i + 1]);
+            I *= func.h / 2;
+            for (size_t i = 0; i < max_iterations && precision < fabsl ((I - I_2h)); i++) {
+                I_2h = I;
+                I = 0;
+                func.build_net (func.netsize + func.netsize - 1);
+                for (size_t i = 0; i < func.netsize - 1; i++)
+                    I += (func.f_data[i] + func.f_data[i + 1]);
+                I *= func.h / 2;
+                iterations = i + 2;
+            }
+            func.build_net (old_netsize);
+            runge = (I - I_2h) / 3;
+            I += runge;
+        }
+        ans = I;
+        return 0;
+    }
+
+    int integrate_simpson ()
+    {
+        if (func.is_improper) {
+            errstr = "improper must use improper methods";
+            return 1;
+        }
+        num_t I = 0;
+        if (func.has_reference) {
+            for (size_t i = 0; i < func.netsize - 1; i++)
+                I += (func.f_data[i] + func.f_data[i + 1] + 4 * func.ref_func((func.x_data[i] + func.x_data[i + 1]) / 2));
+            I *= func.h / 6;
+            if (use_runge) {
+                num_t I_2h = I;
+                I = 0;
+                size_t old_netsize = func.netsize;
+                func.build_net (func.netsize + func.netsize - 1); // x2 number of h-s
+                for (size_t i = 0; i < func.netsize - 1; i++)
+                    I += (func.f_data[i] + func.f_data[i + 1] + 4 * func.ref_func((func.x_data[i] + func.x_data[i + 1]) / 2));
+                I *= func.h / 6;
+                for (size_t i = 0; i < max_iterations && precision < fabsl (I - I_2h); i++) {
+                    I_2h = I;
+                    I = 0;
+                    func.build_net (func.netsize + func.netsize - 1);
+                    for (size_t i = 0; i < func.netsize - 1; i++)
+                        I += func.h * (func.f_data[i] + func.f_data[i + 1] + 4 * func.ref_func((func.x_data[i] + func.x_data[i + 1]) / 2));
+                    I /= 6;
+                    iterations = i + 2;
+                }
+                func.build_net (old_netsize);
+                runge = (I - I_2h) / 15;
+                I += runge;
+            }
+        }
+        else {
+            if (func.netsize < 3) {
+                errstr = "simpson pure net requires at least 3 knots";
+                return 1;
+            }
+            for (size_t i = 0; i < func.netsize - 2; i += 2)
+                I += (func.f_data[i] + func.f_data[i + 2] + 4 * func.f_data[i + 1]);
+            I *= func.h / 3;
+        }
+        ans = I;
+        return 0;
+    }
+
+    int integrate_improper (num_t precision)
+    {
+        if (!func.is_improper) {
+            errstr = "proper must use proper methods";
+            return 1;
+        }
+        num_t I1 = 0, I2 = 0;
+        if (func.end == improper_right) {
+            if (!isnanl (func.x_max)) {
+                num_t edge = func.x_min;
+                do {
+                    edge = (func.x_max + edge) / 2;
+                    I1 = func.eps_func (func.x_max) - func.eps_func (edge);
+                } while (fabsl (I1) > precision / 2);
+                improper_edge = edge;
+            }
+            else {
+                num_t edge = 1;
+                do {
+                    edge *= 2;
+                    I1 = func.eps_func (1e8) - func.eps_func (edge);
+                } while (fabsl (I1) > precision / 2);
+                improper_edge = edge;
+            }
+            netfunc<num_t> proper_f (func.ref_func, func.x_min, improper_edge, 100);
+            integrator<num_t> I2_integrator (proper_f);
+            I2_integrator.enable_runge (precision / 2);
+            I2_integrator.integrate_rectangles ();
+            I2 = I2_integrator.ans;
+        }
+        else {
+            if (!isnanl (func.x_min)) {
+                num_t edge = func.x_max;
+                do {
+                    edge = (func.x_min + edge) / 2;
+                    I1 = func.eps_func (edge) - func.eps_func (func.x_min);
+                } while (fabsl (I1) > precision / 2);
+                improper_edge = edge;
+            }
+            else {
+                num_t edge = -1;
+                do {
+                    edge *= 2;
+                    I1 = func.eps_func (edge) - func.eps_func (-1e8);
+                } while (fabsl (I1) > precision / 2);
+                improper_edge = edge;
+            }
+            netfunc<num_t> proper_f (func.ref_func, improper_edge, func.x_max, 100);
+            integrator<num_t> I2_integrator (proper_f);
+            I2_integrator.enable_runge (precision / 2);
+            I2_integrator.integrate_rectangles ();
+            I2 = I2_integrator.ans;
+        }
+        ans = I1 + I2;
+        return 0;
+    }
+};
---
+++ integrate/main.cpp
@@ -0,0 +1,109 @@
+#include "integrate.h"
+using namespace std;
+
+typedef double flt;
+
+flt fin_left (flt x)
+{
+    return cos (x) / sqrt (x);
+}
+
+flt fin_helper_left (flt x)
+{
+    return 1 * sqrt (x) - sqrt (x * x * x * x * x) / 5;
+}
+
+flt fin_right (flt x)
+{
+    return cos (x) / sqrt (-x);
+}
+
+flt fin_helper_right (flt x)
+{
+    return -1 * sqrt (-x) + sqrt (-x * x * x * x * x) / 5;
+}
+
+flt inf_left (flt x)
+{
+    return exp(x);
+}
+
+flt inf_helper_left (flt x)
+{
+    return exp(x);
+}
+
+flt inf_right (flt x)
+{
+    return exp(-x);
+}
+
+flt inf_helper_right (flt x)
+{
+    return -exp(-x);
+}
+
+int main ()
+{
+    //proper
+    netfunc<flt> f (sin, 0, 2, 5);
+    integrator<flt> I (f);
+    I.integrate_rectangles ();
+    cout << "Integral rectangles = " << I.ans << endl;
+    I.integrate_trapezoids ();
+    cout << "Integral trapezoids = " << I.ans << endl;
+    if (I.integrate_simpson ())
+        cout << I.get_err () << endl;
+    else 
+        cout << "Integral simpson = " << I.ans << endl;
+    I.enable_runge (1e-3);
+    I.integrate_rectangles ();
+    cout << "Integral rectangles runge = " << I.ans << " runge = " << I.runge << " iterations = " << I.iterations << endl;
+    I.integrate_trapezoids ();
+    cout << "Integral trapezoids runge = " << I.ans << " runge = " << I.runge << " iterations = " << I.iterations << endl;
+    if (I.integrate_simpson ())
+        cout << I.get_err () << endl;
+    else
+        cout << "Integral simpson runge = " << I.ans << " runge = " << I.runge << " iterations = " << I.iterations << endl;
+    I.assume_pure_net ();
+    I.integrate_rectangles ();
+    cout << "Integral rectangles pure net = " << I.ans << endl;
+    if (I.integrate_simpson ())
+        cout << I.get_err () << endl;
+    else
+        cout << "Integral simpson pure net = " << I.ans << endl;
+
+    //improper
+    //finite left
+    netfunc<flt> net_fin_left (fin_left, fin_helper_left, 0, 1, improper_left);
+    integrator<flt> Im_fin_left (net_fin_left);
+    if (Im_fin_left.integrate_improper (1e-2))
+        cout << Im_fin_left.get_err () << endl;
+    else
+        cout << "Integral improper finite left = " << Im_fin_left.ans << " edge = " << Im_fin_left.improper_edge << endl;
+    
+    //finite right
+    netfunc<flt> net_fin_right (fin_right, fin_helper_right, -1, 0, improper_right);
+    integrator<flt> Im_fin_right (net_fin_right);
+    if (Im_fin_right.integrate_improper (1e-2))
+        cout << Im_fin_right.get_err () << endl;
+    else
+        cout << "Integral improper finite right = " << Im_fin_right.ans << " edge = " << Im_fin_right.improper_edge << endl;
+
+    //infinite left
+    netfunc<flt> net_inf_left (inf_left, inf_helper_left, NAN, 0, improper_left);
+    integrator<flt> Im_inf_left (net_inf_left);
+    if (Im_inf_left.integrate_improper (1e-2))
+        cout << Im_inf_left.get_err () << endl;
+    else
+        cout << "Integral improper infinite left = " << Im_inf_left.ans << " edge = " << Im_inf_left.improper_edge << endl;
+
+    //infinite right
+    netfunc<flt> net_inf_right (inf_right, inf_helper_right, 0, NAN, improper_right);
+    integrator<flt> Im_inf_right (net_inf_right);
+    if (Im_inf_right.integrate_improper (1e-2))
+        cout << Im_inf_right.get_err () << endl;
+    else
+        cout << "Integral improper infinite right = " << Im_inf_right.ans << " edge = " << Im_inf_right.improper_edge << endl;
+    return 0;
+}
